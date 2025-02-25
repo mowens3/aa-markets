a67f1fe30485986896b401ceea77a4a20bd2b452
Matt Owens <mowens3@gmail.com>
2025-02-24 16:34:12 -0800

    updates to markets tag

--- markets/migrations/0002_marketstag_alter_general_options_and_more.py
+++ markets/migrations/0002_marketstag_alter_general_options_and_more.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name="MetenoxTag",
+            name="MarketsTag",
             fields=[
                 (
                     "id",
